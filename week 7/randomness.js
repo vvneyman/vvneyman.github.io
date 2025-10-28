1936f61bbad0f33454bab9d8c0f3a3aa530f9032
vvneyman <vneyman@risd.edu>
2025-10-27 20:20:04 -0400

    week7 random

---
+++ week 7/randomness.js
@@ -0,0 +1,8 @@
+function weave() {
+    const isBlack = Math.random() < 0.5;
+    
+    const row = document.createElement('div');
+    row.className = 'weave-row ' + (isBlack ? 'black' : 'white');
+    
+    document.getElementById('weaveContainer').appendChild(row);
+}
